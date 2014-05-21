4413ee8ca490e5f77eaceb8bab081e1dcfbce14e
Jameson Nash <vtjnash@gmail.com>
2014-05-21 00:09:43 -0400

    update for changes in Gtk and ?tk.jl?

--- src/gtk.jl
+++ src/gtk.jl
@@ -1,20 +1,42 @@
 import Gtk
 
 function gtkwindow(name, w, h, closecb=nothing)
-    c = Gtk.Canvas()
-    win = Gtk.Window(c, name, w, h)
+    c = Gtk.@Canvas()
+    win = Gtk.@Window(c, name, w, h)
     if closecb !== nothing
         Gtk.on_signal_destroy(closecb, win)
     end
-    c
+    show(c)
 end
 
 function display(c::Gtk.Canvas, pc::PlotContainer)
-    c.draw = function(_)
-        ctx = getgc(c)
-        set_source_rgb(ctx, 1, 1, 1)
-        paint(ctx)
-        Winston.page_compose(pc, Gtk.cairo_surface(c))
+    c.draw = let bad=false
+        function (_)
+            bad && return
+            ctx = getgc(c)
+            set_source_rgb(ctx, 1, 1, 1)
+            paint(ctx)
+            try
+                Winston.page_compose(pc, Gtk.cairo_surface(c))
+            catch e
+                bad = true
+                isa(e, WinstonException) || rethrow(e)
+                println("Winston: ", e.msg)
+            end
+        end
     end
     Gtk.draw(c)
 end
+
+gtkdestroy(c::Gtk.Canvas) = Gtk.destroy(Gtk.toplevel(c))
+
+# JWN: copied the following from tk.jl, but I don't know
+# what it does, so I can't make sure it works
+#
+#function get_context(c::Gtk.Canvas, pc::PlotContainer)
+#    device = CairoRenderer(Gtk.cairo_surface(c))
+#    ext_bbox = BoundingBox(0,width(c),0,height(c))
+#    _get_context(device, ext_bbox, pc)
+#end
+#
+#get_context(pc::PlotContainer) = get_context(curfig(_display), pc)
--- test/canvas.jl
+++ test/canvas.jl
@@ -1,5 +1,18 @@
-using Tk
 using Winston
+if Winston.output_surface == :tk
+    using Tk
+elseif Winston.output_surface == :gtk
+    using Gtk
+else
+    error("unsupported output_surface for this test")
+end
+if Winston.output_surface == :gtk
+    # minimalistic compatibility definitions
+    # must be separate from using Gtk expr above
+    const Toplevel = Gtk.WindowLeaf
+    pack(x...;y...) = nothing
+    Canvas(w, x, y) = ((w |> (c=show(Gtk.@Canvas(x,y)))); c)
+end
 
 win = Toplevel("TestCanvas", 400, 200)
 c = Canvas(win, 400, 200)

396866825cd581b0312e02eeb2aff3f74eef8060
Tim Holy <tim.holy@gmail.com>
2016-10-09 17:21:41 -0500

    Fix for convert->reinterpret with RGB24

--- src/renderer.jl
+++ src/renderer.jl
@@ -59,7 +59,7 @@ end
 
 ## state commands
 
-color_to_rgb(i::Integer) = convert(RGB, RGB24(unsigned(i)))
+color_to_rgb(i::Integer) = convert(RGB, reinterpret(RGB24, UInt32(unsigned(i))))
 color_to_rgb(s::AbstractString) =parse(Colorant,s)
 color_to_rgb(rgb::@compat(Tuple{Real,Real,Real})) = RGB(rgb...)
 color_to_rgb(cv::Color) = convert(RGB, cv)
